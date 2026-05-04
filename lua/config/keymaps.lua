98c938a4905a3470fab4b23f4dda88c50efc2070
Jake Ferrari <jakeferrari93@gmail.com>
2026-05-04 13:09:32 -0700

    fix: change vim terminal escape keys
    - move from <C-\><C-n> to <Esc><Esc>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -33,3 +33,6 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "Make a file executable (UNIX)" })
 
 vim.keymap.set("n", "<leader>h", "<cmd>ColorizerToggle<CR>", { desc = "Toggle Nvim Colorizer" })
+
+-- Remap Term Escape
+vim.keymap.set("t", "<Esc><Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
